b8b24cdde45a08a0b6882adf08f35ebe1a5dc1a6
alekseinedoria <aleksei.nedoria@yandex.ru>
2022-12-11 15:07:20 +0300

    gen entry stub

--- src/genc/gen_module.go
+++ src/genc/gen_module.go
@@ -2,11 +2,23 @@ package genc
 
 import (
 	"fmt"
-	//"trivil/ast"
+	"trivil/ast"
 )
 
 var _ = fmt.Printf
 
 func (genc *genContext) genModule() {
+	genc.genEntry(genc.module.Entry, true)
+}
 
+func (genc *genContext) genEntry(entry *ast.EntryFn, main bool) {
+
+	if !main {
+		panic("ni")
+	}
+
+	genc.c("int main() {")
+
+	genc.c("  return 0;")
+	genc.c("}")
 }
