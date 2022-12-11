9f7e5a8df279cade6632f12a1dcd14d708fd58fa
alekseinedoria <aleksei.nedoria@yandex.ru>
2022-12-11 14:53:33 +0300

    save output

--- src/genc/gen.go
+++ src/genc/gen.go
@@ -2,24 +2,31 @@ package genc
 
 import (
 	"fmt"
+	"os"
+	"path"
+	"strings"
+
 	"trivil/ast"
+	"trivil/env"
 )
 
 var _ = fmt.Printf
 
 type genContext struct {
-	module *ast.Module
-	hlines []string
-	clines []string
+	module  *ast.Module
+	outname string
+	hlines  []string
+	clines  []string
 }
 
 func Generate(m *ast.Module) {
 	fmt.Printf("genc\n")
 
 	var genc = &genContext{
-		module: m,
-		hlines: make([]string, 0),
-		clines: make([]string, 0),
+		module:  m,
+		outname: env.OutName(m.Name),
+		hlines:  make([]string, 0),
+		clines:  make([]string, 0),
 	}
 
 	genc.startCode()
@@ -27,6 +34,8 @@ func Generate(m *ast.Module) {
 	genc.finishCode()
 
 	genc.show()
+	genc.save()
+
 }
 
 func (genc *genContext) h(format string, args ...interface{}) {
@@ -42,12 +51,34 @@ func (genc *genContext) startCode() {
 	genc.h("#ifndef %s", hname)
 	genc.h("#define %s", hname)
 
-	genc.c("#include \"%s\"", "header")
+	genc.c("#include \"%s\"", genc.outname+".h")
 
 }
 
 func (genc *genContext) finishCode() {
 	genc.h("#endif")
+}
+
+//====
+
+func (genc *genContext) save() {
+	var folder = env.PrepareOutFolder()
+
+	writeFile(folder, genc.outname, ".h", genc.hlines)
+	writeFile(folder, genc.outname, ".c", genc.clines)
+}
+
+func writeFile(folder, name, ext string, lines []string) {
+
+	var filename = path.Join(folder, name+ext)
+
+	var out = strings.Join(lines, "\n")
+
+	var err = os.WriteFile(filename, []byte(out), 0755)
+
+	if err != nil {
+		panic("Ошибка записи файла " + filename + ": " + err.Error())
+	}
 }
 
 //====
