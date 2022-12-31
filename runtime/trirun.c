b7c07c32c23f16296ca6b555a5c5def79498dea7
alekseinedoria <aleksei.nedoria@yandex.ru>
2022-12-31 19:59:57 +0300

    runtime strings (stub)

--- runtime/trirun.c
+++ runtime/trirun.c
@@ -1,19 +1,55 @@
 #include "trirun.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-void tri_welcome() {
-  printf("Trivil!\n");
+//==== crash
+
+void crash(char* s) {
+	printf("! crash: %s\n", s);
+	exit(1);
 }
 
+//==== memory
+
+void* mm_allocate(size_t size) {
+	void* a = malloc(size);
+	if (a == NULL) {
+		crash("memory not allocated");
+	}
+	return a;
+}	
+
+//==== strings
+
+TString makeString(uint8_t* body, TInt64 bytes) {
+/*	
+	TString s = mm_allocate(sizeof(TStringDesc));
+	s->lenBytes = bytes;
+	s->lenSymbols = -1;
+*/	
+	
+	return NULL;
+}
+
+//==== console
+
+/*
 void print_int(int i) {
   printf("%d", i);
 }
+*/
 
-void print_int64(int64_t i) {
+void print_int64(TInt64 i) {
   printf("%lld", i);
 }
 
 
 void println() {
   printf("\n");
+}
+
+//==== other
+
+void tri_welcome() {
+  printf("Trivil!\n");
 }
--- runtime/trirun.h
+++ runtime/trirun.h
@@ -1,9 +1,33 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+typedef uint8_t TByte;
+typedef int64_t TInt64;
+typedef double TFloat64;
+typedef _Bool TBool;
+
+
+typedef uint32_t TSymbol;
+
+typedef struct StringDesc {
+  int64_t lenBytes;
+  int64_t lenSymbols;
+  uint8_t* body;
+} StringDesc;
+
+typedef StringDesc* TString;
+
+//====
+
 void tri_welcome();
 
-void print_int(int i);
-void print_int64(int64_t i);
+//==== strings
+
+TString makeString(uint8_t* body, TInt64 bytes);
+
+//==== console
+
+//void print_int(int i);
+void print_int64(TInt64 i);
 
 void println();
