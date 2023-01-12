b8e20fe354e54e8af471e525484bcdad7f70948a
alekseinedoria <aleksei.nedoria@yandex.ru>
2023-01-12 19:24:54 +0300

    fix bat

--- runtime/build.bat
+++ runtime/build.bat
@@ -1,3 +1,1 @@
 clang x.c -llibwelrt -fno-omit-frame-pointer
-
-rem linux: clang x.c -llibwelrt -ldl -lm -o target
--- runtime/rt.bat
+++ runtime/rt.bat
@@ -1,2 +1,1 @@
 clang rt_api.c -ldbghelp --shared -fno-omit-frame-pointer -o libwelrt.dll
-rem linux: *files* -fPIC --shared -fno-omit-frame-pointer -ldl -lm -o libwelrt.so
