3546e8ec0fbff41457eb153844fe62ec5ea010ee
Dmitry Solomennikov <dmitrys99@mail.ru>
2023-04-19 23:29:06 +0300

    Добавлены обработчики сигналов Linux
    Signed-off-by: Dmitry Solomennikov <dmitrys99@mail.ru>

--- runtime/rt_syscrash.c
+++ runtime/rt_syscrash.c
@@ -4,9 +4,182 @@
 
 // ==============   linux     ==============
 
-#ifdef __linux__
+#ifndef _WIN32
 
+#include <err.h>
+#include <execinfo.h>
+#include <stdlib.h>
+#include <stdint.h>
 
+static char const * icky_global_program_name;
+
+/* Resolve symbol name and source location given the path to the executable
+   and an address */
+int addr2line(char const * const program_name, void const * const addr)
+{
+  char addr2line_cmd[512] = {0};
+
+  /* have addr2line map the address to the relent line in the code */
+  #ifdef __APPLE__
+    /* apple does things differently... */
+    sprintf(addr2line_cmd,"atos -o %.256s %p", program_name, addr);
+  #else
+    sprintf(addr2line_cmd,"addr2line -f -p -e %.256s %p", program_name, addr);
+  #endif
+
+  return system(addr2line_cmd);
+}
+
+#define MAX_STACK_FRAMES 64
+
+static void *stack_traces[MAX_STACK_FRAMES];
+void posix_print_stack_trace() {
+    int i, trace_size = 0;
+    char **messages = (char **)NULL;
+
+    trace_size = backtrace(stack_traces, MAX_STACK_FRAMES);
+    messages = backtrace_symbols(stack_traces, trace_size);
+
+    /* skip the first couple stack frames (as they are this function and
+     our handler) and also skip the last frame as it's (always?) junk. */
+    // for (i = 3; i < (trace_size - 1); ++i)
+    for (i = 0; i < trace_size; ++i) {
+  	    // we'll use this for now so you can see what's going on
+        if (addr2line(icky_global_program_name, stack_traces[i]) != 0) {
+            printf("  error determining line # for: %s\n", messages[i]);
+        }
+    }
+
+	if (messages) {
+	    free(messages);
+	}
+}
+
+void posix_signal_handler(int sig, siginfo_t *siginfo, void *context) {
+    (void)context;
+    switch(sig) {
+    case SIGSEGV:
+        fputs("Caught SIGSEGV: Segmentation Fault\n", stderr);
+        break;
+	case SIGINT:
+        fputs("Caught SIGINT: Interactive attention signal, (usually ctrl+c)\n", stderr);
+        break;
+	case SIGFPE:
+	    switch(siginfo->si_code) {
+		case FPE_INTDIV:
+            fputs("Caught SIGFPE: (integer divide by zero)\n", stderr);
+            break;
+		case FPE_INTOVF:
+            fputs("Caught SIGFPE: (integer overflow)\n", stderr);
+            break;
+		case FPE_FLTDIV:
+            fputs("Caught SIGFPE: (floating-point divide by zero)\n", stderr);
+            break;
+		case FPE_FLTOVF:
+            fputs("Caught SIGFPE: (floating-point overflow)\n", stderr);
+            break;
+		case FPE_FLTUND:
+            fputs("Caught SIGFPE: (floating-point underflow)\n", stderr);
+            break;
+		case FPE_FLTRES:
+            fputs("Caught SIGFPE: (floating-point inexact result)\n", stderr);
+            break;
+		case FPE_FLTINV:
+            fputs("Caught SIGFPE: (floating-point invalid operation)\n", stderr);
+            break;
+		case FPE_FLTSUB:
+            fputs("Caught SIGFPE: (subscript out of range)\n", stderr);
+            break;
+		default:
+            fputs("Caught SIGFPE: Arithmetic Exception\n", stderr);
+            break;
+        }
+	case SIGILL:
+	    switch(siginfo->si_code) {
+		case ILL_ILLOPC:
+            fputs("Caught SIGILL: (illegal opcode)\n", stderr);
+            break;
+		case ILL_ILLOPN:
+            fputs("Caught SIGILL: (illegal operand)\n", stderr);
+            break;
+		case ILL_ILLADR:
+            fputs("Caught SIGILL: (illegal addressing mode)\n", stderr);
+            break;
+		case ILL_ILLTRP:
+            fputs("Caught SIGILL: (illegal trap)\n", stderr);
+            break;
+		case ILL_PRVOPC:
+            fputs("Caught SIGILL: (privileged opcode)\n", stderr);
+            break;
+		case ILL_PRVREG:
+            fputs("Caught SIGILL: (privileged register)\n", stderr);
+            break;
+		case ILL_COPROC:
+            fputs("Caught SIGILL: (coprocessor error)\n", stderr);
+            break;
+		case ILL_BADSTK:
+            fputs("Caught SIGILL: (internal stack error)\n", stderr);
+            break;
+		default:
+            fputs("Caught SIGILL: Illegal Instruction\n", stderr);
+            break;
+        }
+        break;
+	case SIGTERM:
+        fputs("Caught SIGTERM: a termination request was sent to the program\n", stderr);
+        break;
+	case SIGABRT:
+        fputs("Caught SIGABRT: usually caused by an abort() or assert()\n", stderr);
+        break;
+	default:
+        break;
+    }
+    posix_print_stack_trace();
+    exit(1);
+  }
+
+static uint8_t alternate_stack[SIGSTKSZ];
+void set_signal_handler() {
+    /* setup alternate stack */
+    {
+        stack_t ss = {};
+        /* malloc is usually used here, I'm not 100% sure my static allocation
+           is valid but it seems to work just fine. */
+        ss.ss_sp = (void*)alternate_stack;
+        ss.ss_size = SIGSTKSZ;
+        ss.ss_flags = 0;
+
+        if (sigaltstack(&ss, NULL) != 0) {
+		  err(1, "sigaltstack");
+		}
+    }
+
+    /* register our signal handlers */
+    {
+        struct sigaction sig_action = {};
+        sig_action.sa_sigaction = posix_signal_handler;
+        sigemptyset(&sig_action.sa_mask);
+
+        #ifdef __APPLE__
+            /* for some reason we backtrace() doesn't work on osx
+               when we use an alternate stack */
+            sig_action.sa_flags = SA_SIGINFO;
+        #else
+            sig_action.sa_flags = SA_SIGINFO | SA_ONSTACK;
+        #endif
+
+		if (sigaction(SIGSEGV, &sig_action, NULL) != 0) { err(1, "sigaction"); }
+        if (sigaction(SIGFPE,  &sig_action, NULL) != 0) { err(1, "sigaction"); }
+        if (sigaction(SIGINT,  &sig_action, NULL) != 0) { err(1, "sigaction"); }
+        if (sigaction(SIGILL,  &sig_action, NULL) != 0) { err(1, "sigaction"); }
+        if (sigaction(SIGTERM, &sig_action, NULL) != 0) { err(1, "sigaction"); }
+        if (sigaction(SIGABRT, &sig_action, NULL) != 0) { err(1, "sigaction"); }
+    }
+}
+
+EXPORTED void register_default_crash_handler() {
+    set_signal_handler();
+}
 
 // ============== windows ==============
 
