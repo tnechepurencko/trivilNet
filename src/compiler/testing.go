0e7f7047ff2a84cd67efa46bb9db1523b1e792f7
alekseinedoria <aleksei.nedoria@yandex.ru>
2023-04-22 23:21:36 +0300

    режим тестирования (в работе)
    Signed-off-by: alekseinedoria <aleksei.nedoria@yandex.ru>

--- src/compiler/testing.go
+++ src/compiler/testing.go
@@ -2,25 +2,48 @@ package compiler
 
 import (
 	"fmt"
+
+	//	"trivil/ast"
+	"trivil/env"
 	/*
-		"trivil/ast"
-		"trivil/env"
 		"trivil/genc"
 		"trivil/semantics"
 	*/)
 
 var _ = fmt.Printf
 
-func TestOne(spath string) {
-	fmt.Printf("тест %v\n", spath)
+const testsFolderName = "_тест_"
+
+func TestOne(tpath string) {
+	fmt.Printf("тест %v\n", tpath)
+
+	env.Normalizer.Process(tpath)
+	if env.Normalizer.Err != nil {
+		env.FatalError("ОКР-ОШ-ПУТЬ-ТЕСТ", tpath, env.Normalizer.Err.Error())
+		return
+	}
+
+	var npath = env.Normalizer.NPath
+
+	var err = env.EnsureFolder(npath)
+	if err != nil {
+		env.FatalError("ОКР-ТЕСТ-НЕ-ПАПКА", tpath, err.Error())
+		return
+	}
+
+	var list = env.GetFolderSources(tpath, npath)
+
+	if len(list) == 0 {
+		env.FatalError("ОКР-ТЕСТ-ПУСТАЯ-ПАПКА", tpath, npath)
+		return
+	}
+
+	if len(list) == 1 && list[0].Err != nil {
+		env.FatalError("ОКР-ОШ-ЧТЕНИЕ-ИСХОДНОГО", list[0].FilePath, list[0].Err.Error())
+		return
+	}
 
 	/*
-		var list = env.GetSources(spath)
-		var src = list[0]
-		if src.Err != nil {
-			env.FatalError("ОКР-ОШ-ЧТЕНИЕ-ИСХОДНОГО", spath, src.Err.Error())
-			return
-		}
 
 		var cc = &compileContext{
 			imported: make(map[string]*ast.Module),
